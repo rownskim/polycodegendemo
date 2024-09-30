a557bf0da82f08dc58499eeb131bd56d51b4bef3
rownskim <mariusz.rowny@smartbear.com>
2024-09-30 15:19:29 +0200

    init commit

--- src/main/java/com/smartbear/swaggerhub/portal/polycodegendemo/ContentTableOfContentsApiImpl.java
+++ src/main/java/com/smartbear/swaggerhub/portal/polycodegendemo/ContentTableOfContentsApiImpl.java
@@ -3,11 +3,20 @@ package com.smartbear.swaggerhub.portal.polycodegendemo;
 import com.smartbear.swaggerhub.portal.polycodegendemo.apimodel.generated.controller.ContentTableOfContentsApi;
 import com.smartbear.swaggerhub.portal.polycodegendemo.apimodel.generated.model.BaseTableOfContentsItem;
 import com.smartbear.swaggerhub.portal.polycodegendemo.apimodel.generated.model.CreateTableOfContentsEntry201Response;
+import com.smartbear.swaggerhub.portal.polycodegendemo.apimodel.generated.model.TableOfContentsItemCopy;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ContentTableOfContentsApiImpl implements ContentTableOfContentsApi {
     @Override
     public ResponseEntity<CreateTableOfContentsEntry201Response> createTableOfContentsEntry(BaseTableOfContentsItem baseTableOfContentsItem) throws Exception {
-        return null;
+        if (baseTableOfContentsItem instanceof TableOfContentsItemCopy) {
+            return ResponseEntity.ok(new CreateTableOfContentsEntry201Response().helloMsg("Hello, copied item"));
+        } else if (baseTableOfContentsItem instanceof BaseTableOfContentsItem) {
+            return ResponseEntity.ok(new CreateTableOfContentsEntry201Response().helloMsg("Hello, new item"));
+        } else {
+            throw new Exception("Invalid request");
+        }
     }
 }
